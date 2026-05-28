7f26006131fee9efc5a1c6cdefb700160c994cec
Rathodnimesh <rathodnimesh2007@gmail.com>
2026-05-28 15:38:35 +0530

    software_enginnering

---
+++ Assignments/sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+float main()
+{
+	float a=20;
+	 float b=300;
+	
+	printf("\n sum %0.2f",a+b);
+	printf("\n sub %0.2f",a-b);
+	printf("\n mul %0.2f",a*b);
+	printf("\n div %0.2f",a/b);
+	
+	getch ();
+}
